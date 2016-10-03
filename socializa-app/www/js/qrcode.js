3dbf8d55d56e2e63d8506a1c5f74bdd55e739739
Victor Ramirez <virako@wadobo.com>
2016-10-03 08:00:25 +0200

    Uploaded forget js file

---
+++ socializa-app/www/js/qrcode.js
@@ -0,0 +1,25 @@
+function scanQR() {
+    cordova.plugins.barcodeScanner.scan(
+            function(result) {
+                document.getElementById("qrtext").innerHTML = result.text;
+                alert(JSON.stringify(result.text));
+                localStorage.setItem("LocalData", JSON.stringify(result.text));
+            }, function(error) {
+                document.getElementById("qrcreating").innerHTML = error;
+                alert("ERROR", error);
+            }
+    );
+}
+
+function createQR() {
+    alert(localStorage.getItem("LocalData"));
+    cordova.plugins.barcodeScanner.encode(
+            cordova.plugins.barcodeScanner.Encode.TEXT_TYPE,
+            "01EMPQVERI0NEPE",
+            function(success) {
+                document.getElementById("creating").innerHTML = "success";
+            }, function(fail) {
+                document.getElementById("creating").innerHTML = "fail";
+            }
+    );
+}
