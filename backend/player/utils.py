82588ef283b9921601cd242c4081d6aea2fb5655
Victor Ramirez <virako@wadobo.com>
2016-12-16 17:36:18 +0100

    Fixed get email from google authentication.

--- backend/player/utils.py
+++ backend/player/utils.py
@@ -2,7 +2,11 @@ from .models import Player
 
 
 def create_player(backend, user, response, *args, **kwargs):
-    player = Player.objects.get_or_create(user=user)
-    if not player:
-        player = Player(user=user)
-        player.save()
+    if backend.name == "google-oauth2":
+        player, new = Player.objects.get_or_create(user=user)
+        if new and player:
+            player.save()
+    elif backend.name == "facebook":
+        pass
+    elif backend.name == "twitter":
+        pass
