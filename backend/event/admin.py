3dba0a3e345807baf2f5ca326a8fda60b471c236
Victor Ramirez <virako@wadobo.com>
2017-01-16 12:37:59 +0100

    Map with street in admin Event when select polygon

--- backend/event/admin.py
+++ backend/event/admin.py
@@ -1,11 +1,20 @@
-from django.contrib import admin
+from django.contrib.gis import admin
+from django.contrib.gis.geos import Point
 
 from .models import Event
 from .models import Membership
 
 
-class EventAdmin(admin.ModelAdmin):
+class EventAdmin(admin.OSMGeoAdmin):
     list_display = ('name', 'game', 'status', 'price', 'start_date', 'end_date')
+    # Center in Spain
+    default_zoom = 6
+    pnt = Point(-4, 40, srid=4326)
+    pnt.transform(3857) # tranform to google SRID
+    default_lon, default_lat = pnt.coords
+    # Map size
+    map_width = 800
+    map_height = 600
 
 
 class MembershipAdmin(admin.ModelAdmin):
