7ce5e66cc0640b12e0a070afe52b608fab4daa59
Daniel García Moreno <danigm@wadobo.com>
2017-03-07 17:59:07 +0100

    backend: Fixed related_name in clue model

--- backend/clue/models.py
+++ backend/clue/models.py
@@ -13,8 +13,11 @@ CLUE_STATUS = (
 
 
 class Clue(models.Model):
-    player = models.ForeignKey(Player, on_delete=models.CASCADE, related_name="player")
-    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="event", default=None)
-    challenge = models.ForeignKey(Challenge, on_delete=models.CASCADE, related_name="challenge")
+    player = models.ForeignKey(Player, on_delete=models.CASCADE, related_name="clues")
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="clues", default=None)
+    challenge = models.ForeignKey(Challenge, on_delete=models.CASCADE, related_name="clues")
     main = models.BooleanField(default=False)
     status = models.CharField(max_length=16, choices=CLUE_STATUS, default='acquired')
+
+    def __str__(self):
+        return '<%s - %s - %s>' % (self.player, self.event, self.challenge)
