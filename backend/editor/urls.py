fe943d089143e1e0e5a6e67bc2342caa956654fa
Daniel García Moreno <danigm@wadobo.com>
2017-03-13 10:49:43 +0100

    backend: Started the event editor

--- backend/editor/urls.py
+++ backend/editor/urls.py
@@ -6,4 +6,8 @@ from . import views
 urlpatterns = [
     url(r'^game/$', views.edit_game, name="add_game"),
     url(r'^game/(?P<gameid>\d+)/$', views.edit_game, name="edit_game"),
+
+    url(r'^event/$', views.edit_event, name="add_event"),
+    url(r'^event/(?P<evid>\d+)/$', views.edit_event, name="edit_event"),
+    url(r'^event/challenges/(?P<evid>\d+)/$', views.event_challenges, name="event_challenges"),
 ]
