75e9febbd98d1ff76d74605db0bb96d23aa82b0f
Daniel García Moreno <danigm@wadobo.com>
2017-03-16 09:11:00 +0100

    editor: Removed map position from game definition

--- backend/editor/static/edit_game.js
+++ backend/editor/static/edit_game.js
@@ -1,6 +1,3 @@
-var map = null;
-var selectedPosInput = null;
-
 function addchallenge() {
     var ch = $(".orig_challenge").last().clone();
     var n = parseInt(ch.attr("n"), 10);
@@ -19,7 +16,6 @@ function addchallenge() {
     $("#challenges").append(ch);
 
     $(".rmchallenge").unbind("click").click(rmchallenge);
-    $(".mapbtn").unbind("click").click(showmap);
     return false;
 }
 
@@ -29,50 +25,7 @@ function rmchallenge() {
     return false;
 }
 
-function pointSelected(evt) {
-    var c = evt.coordinate;
-    var p = new ol.proj.transform([c[0], c[1]], 'EPSG:3857', 'EPSG:4326');
-    selectedPosInput.val("" + p[1] + ", " + p[0]);
-    $("#posModal").modal("hide");
-}
-
-function showmap() {
-    selectedPosInput = $(this).parent().parent().find(".pos");
-    $("#posModal").modal("show");
-    if (!map) {
-        setTimeout(function() {
-            map = new ol.Map({
-              target: 'map',
-              layers: [
-                new ol.layer.Tile({
-                  source: new ol.source.OSM()
-                })
-              ],
-              view: new ol.View({
-                center: ol.proj.fromLonLat([-5.9866369, 37.3580539]),
-                zoom: 14
-              })
-            });
-
-            var geocoder = new Geocoder('nominatim', {
-              provider: 'osm',
-              //key: '__some_key__',
-              lang: 'es-ES',
-              placeholder: 'Search for ...',
-              targetType: 'text-input',
-              limit: 5,
-              keepOpen: true
-            });
-            map.addControl(geocoder);
-
-            map.on("singleclick", pointSelected);
-        }, 500);
-    }
-    return false;
-}
-
 $(document).ready(function() {
     $("#addchallenge").click(addchallenge);
-    $(".mapbtn").click(showmap);
     $(".rmchallenge").unbind("click").click(rmchallenge);
 });
