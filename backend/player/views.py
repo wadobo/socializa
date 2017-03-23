cf625506010f8661da0f53417d73994cb93043e3
Daniel García Moreno <danigm@wadobo.com>
2017-03-23 13:09:43 +0100

    backend: Added challenge dependencies

--- backend/player/views.py
+++ backend/player/views.py
@@ -86,8 +86,10 @@ near = PlayersNear.as_view()
 class MeetingCreate(APIView):
 
     def create_clue(self, challenge):
+        # TODO: avoid clue duplication
         clue = Clue(player=self.player1, event=self.event, challenge=challenge)
         clue.save()
+        return clue
 
     @classmethod
     def get_challenge(cls, player, event):
@@ -144,6 +146,83 @@ class MeetingCreate(APIView):
 
         return {}, None
 
+    def give_clue(self, player, event):
+        challenge = self.get_challenge(player, event)
+        if not challenge:
+            return None
+
+        # challenge dependencies
+        pclues = self.player1.clues
+        for dep in challenge.depends.all():
+            if not pclues.filter(challenge=dep, event=self.event).exists():
+                return None
+
+        return self.create_clue(challenge)
+
+    def check_secret(self, event_id, secret):
+        query = Q(event_id__isnull=True) if event_id is None else Q(event_id=event_id)
+        query &= Q(player1=self.player1, player2=self.player2, status='step2', secret=secret)
+
+        response = "Invalid secret"
+        status = rf_status.HTTP_400_BAD_REQUEST
+        clue = None
+
+        meeting = Meeting.objects.filter(query).first()
+        if meeting:
+            response = {}
+            status = rf_status.HTTP_200_OK
+
+            clue = self.give_clue(self.player2, self.event)
+            response['clue'] = ChallengeSerializer(clue.challenge).data if clue else {}
+
+            # always connected if the secret is ok
+            meeting.status = 'connected'
+            meeting.save()
+            response['status'] = meeting.status
+
+        return Response(response, status)
+
+    def check_steps(self, event_id):
+        response = {'status': 'already connected'}
+        status = rf_status.HTTP_200_OK
+
+        query = Q(event_id__isnull=True) if event_id is None else Q(event_id=event_id)
+        query1 = query & Q(player1=self.player1, player2=self.player2)
+        query2 = query & Q(player1=self.player2, player2=self.player1)
+        meeting1 = Meeting.objects.filter(query1).first()
+        meeting2 = Meeting.objects.filter(query2).first()
+
+        m = None
+
+        # STEP1: exception player2 is ai
+        if self.player2.ptype == 'ai':
+            m = create_meeting(self.player1, self.player2, event_id)
+            status = rf_status.HTTP_201_CREATED
+            clue = self.give_clue(self.player2, self.event)
+            response['clue'] = ChallengeSerializer(clue.challenge).data if clue else {}
+
+        # STEP1: player1 not connected with player2 or vice versa
+        elif not meeting1 and not meeting2:
+            m = create_meeting(self.player1, self.player2, event_id)
+            status = rf_status.HTTP_201_CREATED
+
+        elif meeting1:
+            m = meeting1
+            m.status = 'step1'
+            m.save()
+
+        # STEP2: player1 has created meeting with status step1
+        elif meeting2:
+            m = meeting2
+            m.status = 'step2'
+            m.generate_secret()
+            m.save()
+            response['secret'] = m.secret
+
+        response['status'] = m.status if m else ""
+
+        return Response(response, status)
+
     def post(self, request, player_id, event_id=None, secret=None):
         """ For create a meeting there are 4 steps:
             1: player1 try to connect with player2 (create meeting with status step1)
@@ -152,8 +231,6 @@ class MeetingCreate(APIView):
             3: player1 check the secret of meeting (if correct, status connected and return clue)
             4: player2 make polling for check status (if correct, return clue)
         """
-        response = {}
-        status = rf_status.HTTP_200_OK
         self.player1 = None
         self.player2 = None
         self.event = None
@@ -164,57 +241,14 @@ class MeetingCreate(APIView):
             return Response(response, status=status)
 
         if not secret:
-            # Check step
-            query = Q(event_id__isnull=True) if event_id is None else Q(event_id=event_id)
-            query1 = query & Q(player1=self.player1, player2=self.player2)
-            query2 = query & Q(player1=self.player2, player2=self.player1)
-            meeting1 = Meeting.objects.filter(query1).first()
-            meeting2 = Meeting.objects.filter(query2).first()
-
-            if self.player2.ptype == 'ai':  # STEP1: exception player2 is ai
-                meeting = create_meeting(self.player1, self.player2, event_id)
-                status = rf_status.HTTP_201_CREATED
-                challenge = self.get_challenge(self.player2, self.event)
-                self.create_clue(challenge)
-                response['status'] = meeting.status
-                response['clue'] = ChallengeSerializer(challenge).data
-            elif not meeting1 and not meeting2:  # STEP1: player1 not connect with player2 or vice versa
-                meeting = create_meeting(self.player1, self.player2, event_id)
-                response['status'] = 'step1'
-                status = rf_status.HTTP_201_CREATED
-            elif meeting1:
-                response['status'] = 'step1'
-            elif meeting2:  # STEP2: player1 has created meeting with status step1
-                meeting2.generate_secret()
-                response['status'] = 'step2'
-                response['secret'] = meeting2.secret
-            else:
-                response['status'] = 'already connected'
+            return self.check_steps(event_id)
         else: # STEP 3
-            query = Q(event_id__isnull=True) if event_id is None else Q(event_id=event_id)
-            query &= Q(player1=self.player1, player2=self.player2, status='step2', secret=secret)
-            meeting = Meeting.objects.filter(query).first()
-            if meeting:
-                meeting.status = 'connected'
-                meeting.save()
-                status = rf_status.HTTP_200_OK
-                response['status'] = meeting.status
-                challenge = self.get_challenge(self.player2, self.event)
-                if challenge:
-                    self.create_clue(challenge)
-                    response['clue'] = ChallengeSerializer(challenge).data
-                else:
-                    response['clue'] = {}
-            else:
-                response = "Invalid secret"
-                status = rf_status.HTTP_400_BAD_REQUEST
+            return self.check_secret(event_id, secret)
 
         return Response(response, status=status)
 
     def get(self, request, player_id, event_id=None):
         # STEP 4
-        response = {}
-        status = rf_status.HTTP_200_OK
         self.player1 = None
         self.player2 = None
         self.event = None
@@ -227,18 +261,16 @@ class MeetingCreate(APIView):
         query = Q(event_id__isnull=True) if event_id is None else Q(event_id=event_id)
         query &= Q(player1=self.player2, player2=self.player1)
         meeting = Meeting.objects.filter(query).first()
+
         if not meeting:
             response = "Meeting not exist"
             status = rf_status.HTTP_400_BAD_REQUEST
+
         elif meeting.status == 'connected':
-            challenge = self.get_challenge(self.player2, self.event)
+            clue = self.give_clue(self.player2, self.event)
             response['status'] = meeting.status
-            challenge = self.get_challenge(self.player2, self.event)
-            if challenge:
-                self.create_clue(challenge)
-                response['clue'] = ChallengeSerializer(challenge).data
-            else:
-                response['clue'] = {}
+            response['clue'] = ChallengeSerializer(clue.challenge).data if clue else {}
+
         elif meeting.status == 'step2':
             response['status'] = 'waiting'
 
