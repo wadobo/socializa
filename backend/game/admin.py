45501bad8d3ee29c89def1b2a9c41b932ec98794
Daniel García Moreno <danigm@wadobo.com>
2017-04-29 20:39:33 +0200

    backend: Better admin for game and challenge

--- backend/game/admin.py
+++ backend/game/admin.py
@@ -4,10 +4,25 @@ from .models import Challenge
 from .models import Game
 
 
+class GameInline(admin.TabularInline):
+    model = Challenge.games.through
+
+
 class ChallengeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'desc')
+    list_display = ('name', 'desc', 'solution', 'childs', 'type', 'depend')
     filter_horizontal = ('depends', 'child_challenges')
 
+    list_filter = ('ctype', 'games')
+    inlines = [GameInline]
+
+    def depend(self, obj):
+        return ', '.join(i.name for i in obj.depends.all())
+
+    def childs(self, obj):
+        return ', '.join(i.name for i in obj.child_challenges.all())
+
+    def type(self, obj):
+        return obj.ctype
 
 class GameAdmin(admin.ModelAdmin):
     list_display = ('name', 'desc', 'solution')
